cfe7efcffe6e15032584e035d4ce98b0d144af0d
Tima Tolochko <timur10645@icloud.com>
2016-07-13 20:05:47 +0300

    delegate example2

---
+++ DesignPatterns/src/Delegate/DelegateExample2.java
@@ -0,0 +1,50 @@
+package Delegate;
+
+import java.util.Scanner;
+
+public class DelegateExample2 {
+
+    public static void main(String[] args) {
+        System.out.println("Введите программиста");
+        Scanner scanner = new Scanner(System.in);
+        String programmerType = scanner.nextLine();
+        Programmer programmer = new Programmer();
+
+        if(programmerType.equalsIgnoreCase("Java programmer")){
+            programmer.programming = new JavaProgramming();
+        }else if(programmerType.equalsIgnoreCase("C# programmer")){
+            programmer.programming = new CSharpProgramming();
+        }
+        programmer.makeCode();
+    }
+}
+
+
+
+interface Programming{
+    void makeCode();
+}
+
+class JavaProgramming implements Programming{
+    public void makeCode(){
+        System.out.println("JavaVirtualMachine, Oracle");
+    }
+}
+
+class CSharpProgramming implements Programming{
+    public void makeCode(){
+        System.out.println(".NET, Microsoft");
+    }
+}
+
+
+class Programmer {
+    Programming programming;
+
+    void makeCode(){
+        programming.makeCode();
+    }
+}
+
+
+
