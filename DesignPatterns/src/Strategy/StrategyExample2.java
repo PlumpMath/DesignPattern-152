cbdda38ea6eb09a8f683e8f220005e4274fe82f1
Tima Tolochko <timur10645@icloud.com>
2016-07-15 15:56:26 +0300

    Bubble sort Inserting sort Selection sort

--- DesignPatterns/src/Strategy/StrategyExample2.java
+++ DesignPatterns/src/Strategy/StrategyExample2.java
@@ -1,8 +1,30 @@
 package Strategy;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class StrategyExample2 {
+    public static void main(String[] args) {
+        int[] arr1 = new int[10];
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = new Random().nextInt(100);
+        }
+        StrategySorting client = new StrategySorting();
+        client.setStrategy(new BubbleSort());
+        client.performSort(arr1);
+        int[] arr2 = new int[10];
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = new Random().nextInt(100);
+        }
+        client.setStrategy(new InsertingSort());
+        client.performSort(arr2);
+        int[] arr3 = new int[10];
+        for (int i = 0; i < arr3.length; i++) {
+            arr3[i] = new Random().nextInt(100);
+        }
+        client.setStrategy(new SelectionSort());
+        client.performSort(arr3);
+    }
 }
 
 class StrategySorting{
@@ -48,6 +70,22 @@ class SelectionSort implements Sorting{
                     }
                 }
             }
+        System.out.println("After:\t"+Arrays.toString(arr));
+    }
+}
+class InsertingSort implements Sorting{
+    public void sort(int[] arr){
+        System.out.println("SelectionSort");
+        System.out.println("before:\t"+ Arrays.toString(arr));
+        for (int barrier = 1; barrier < arr.length ; barrier++) {
+            int index = barrier;
+            while (index - 1 >= 0 && arr[index] < arr[index-1]){
+                int tmp = arr[index];
+                arr[index] = arr[index-1];
+                arr[index-1] = tmp;
+                index--;
+            }
+        }
         System.out.println("After:\t"+Arrays.toString(arr));
     }
 }
