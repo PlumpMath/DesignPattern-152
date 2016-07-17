09840c72ca15b3a95b0b6c91d149c5ab6cc8701e
Tima Tolochko <timur10645@icloud.com>
2016-07-17 14:06:22 +0300

    IO example of Decorator

---
+++ DesignPatterns/src/DecoratorExample/io/InputTest.java
@@ -0,0 +1,20 @@
+package DecoratorExample.io;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class InputTest {
+
+    public static void main(String[] args) throws IOException{
+        int c;
+        try(InputStream in = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream("E:\\HomeWorks\\HomeWork\\DesignPatterns\\src\\DecoratorExample\\io\\test")))){
+            while ((c= in.read()) >= 0){
+                System.out.print((char) c);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
