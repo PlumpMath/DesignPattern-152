a558c19dfc06dc3e26031463bfaefc6b8396474f
Tima Tolochko <timur10645@icloud.com>
2016-07-18 18:39:29 +0300

    new realization

--- DesignPatterns/src/FactoryExampleWatchFactoryMethod/MainTest.java
+++ DesignPatterns/src/FactoryExampleWatchFactoryMethod/MainTest.java
@@ -2,11 +2,20 @@ package FactoryExampleWatchFactoryMethod;
 
 public class MainTest {
     public static void main(String[] args) {
-        WatchFactory factory = new RomanWatchFactory();
-
+        WatchFactory factory = getWatch("Roman");
         Watch watch = factory.createWatch();
         watch.showTime();
 
 
+    }
+
+    public static WatchFactory getWatch(String watch){
+
+        if(watch.equalsIgnoreCase("Roman")){
+            return new RomanWatchFactory();
+        }else if(watch.equalsIgnoreCase("digital")){
+            return new DigitalWatchFactory();
+        }
+        return null;
     }
 }
