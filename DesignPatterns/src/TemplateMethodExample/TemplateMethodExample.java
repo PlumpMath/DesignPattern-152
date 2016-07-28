ee3e9b2ccfbd56d0981e76c87ec5b418d69c6530
Tima Tolochko <timur10645@icloud.com>
2016-07-28 19:05:19 +0300

    testing the coffee

--- DesignPatterns/src/TemplateMethodExample/TemplateMethodExample.java
+++ DesignPatterns/src/TemplateMethodExample/TemplateMethodExample.java
@@ -2,4 +2,8 @@ package TemplateMethodExample;
 
 
 public class TemplateMethodExample {
+    public static void main(String[] args) {
+        Coffee coffee = new Coffee();
+        coffee.prepareRecipe();
+    }
 }
