57f3907aad92f72d1448d40d93ed9bedc31b2d40
Tima Tolochko <timur10645@icloud.com>
2016-07-29 17:58:55 +0300

    Quizfull

---
+++ DesignPatterns/src/App10.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Тимур on 29.07.2016.
+ */
+public class App10 extends  Thread {
+    public static void main(String[] args) {
+
+        Integer i1 = 1000;
+        Integer i2 = 1000;
+        System.out.println(i1 == i2);
+        System.out.println(i1 == 1000);
+    }
+}
