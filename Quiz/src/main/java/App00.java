114edbcb3fdde5d52e005999f29d6a4d5eb885e2
Tima Tolochko <timur10645@icloud.com>
2016-07-29 18:03:07 +0300

    init commit

---
+++ Quiz/src/main/java/App00.java
@@ -0,0 +1,11 @@
+
+public class App00 {
+    public static void main(String[] args) {
+        Integer i1 = 1000;
+        Integer i2 = 1000;
+
+        System.out.println(i1 == i2);
+        System.out.println(i1 == 1000);
+
+    }
+}
