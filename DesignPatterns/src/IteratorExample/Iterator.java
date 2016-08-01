30cf01518790b99158ae6d0a69eeb22e3cf4e3aa
Tima Tolochko <timur10645@icloud.com>
2016-08-01 22:11:22 +0300

    no change

--- DesignPatterns/src/IteratorExample/Iterator.java
+++ DesignPatterns/src/IteratorExample/Iterator.java
@@ -2,8 +2,6 @@ package IteratorExample;
 
 
 public interface Iterator {
-
-
     boolean hasNext();  // возвращает флаг который указывает, остались ли в коллекции елементы
     Object next();      // возвращает след елемент
 }
