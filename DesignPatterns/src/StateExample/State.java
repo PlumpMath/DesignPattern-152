054a80eb58f963fa22731ee9dd7fbba5e6f08424
Tima Tolochko <timur10645@icloud.com>
2016-08-03 16:51:18 +0300

    init

---
+++ DesignPatterns/src/StateExample/State.java
@@ -0,0 +1,11 @@
+package StateExample;
+
+
+public class State {
+    final static int SOLD_OUT = 0;
+    final static int NO_QUARTER = 1;
+    final static int HAS_QUARTER = 2;
+    final static int SOLD = 3;
+
+    int state = SOLD_OUT;
+}
