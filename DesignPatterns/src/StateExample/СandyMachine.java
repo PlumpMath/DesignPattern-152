662836887bda36cbd9d63d83bd40ee91d1080da6
Tima Tolochko <timur10645@icloud.com>
2016-08-05 11:05:19 +0300

    метод бросить монетку

--- DesignPatterns/src/StateExample/СandyMachine.java
+++ DesignPatterns/src/StateExample/СandyMachine.java
@@ -1,5 +1,29 @@
 package StateExample;
 
 
-public class СandyMachine {
+public class СandyMachine extends State{
+
+
+    public void insertQuarter(){
+        if(state == HAS_QUARTER){
+
+            System.out.println("You can't insert another quarter");
+
+        } else if(state == SOLD_OUT){
+
+            System.out.println("You can't insert a quarter, the machine is sold out");
+
+        } else if(state == SOLD){
+
+            System.out.println("Please wait, we're already giving you a gumball");
+
+        } else if(state == NO_QUARTER){
+
+            state = HAS_QUARTER;
+            System.out.println("You inserted a quarter");
+
+        }
+
+
+    }
 }
